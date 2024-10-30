66cad106a1021656f4a4b73c54aa8f2857984319
Foows - (Dave) <dbangcoyo11@gmail.com>
2024-10-30 10:39:24 +0800

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,16 @@
+#dave.py
+
+import os
+
+def clear():
+    if os.system == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
 def main():
+    clear()
     print(f""" 
     ╔═════════════════════════╗
     ║       CALCULATOR        ║
@@ -10,5 +22,21 @@ def main():
     ║  5.   All in One        ║
     ║  0.   Exit              ║
     ╚═════════════════════════╝""")
+    choice = int(input("     Choice: "))
+
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    elif choice == 5:
+        pass
+    elif choice == 0:
+        pass
+    else:
+        print("     Invalid input!")
 
 main()
