7052098ef8fe29bc69b3bf203e5ed02538bb2b0b
Foows - (Dave) <dbangcoyo11@gmail.com>
2024-11-03 21:43:00 +0800

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,12 @@
 #dave.py
 
+#Color
+#Border = \x1b[1;36m
+#Title = \x1b[1;33m
+#Text = \x1b[1;32m
+#Command = \x1b[1;37m
+#Back = \x1b[1;37m
+
 import os
 import time
 import math
@@ -13,11 +20,11 @@ def clear():
 def add():
     global num1, num2, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║        ADDITION         ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
-    num2 = int(input("     \x1b[1;32mNum2: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║        \x1b[1;33mADDITION         \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mNum2:\x1b[1;37m "))
     result = num1 + num2
     time.sleep(1)
     clear()
@@ -25,41 +32,39 @@ def add():
 
 def add_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║     ADDITION RESULT     ║
-    ╠═════════════════════════╣
-    ║       Num1 = {num1:<10} ║
-    ║       Num2 = {num2:<10} ║
-    ║       Total = {result:<10}║
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║     \x1b[1;31mADDITION RESULT     \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║       \x1b[1;32mNum1 = \x1b[1;37m{num1:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mNum2 = \x1b[1;37m{num2:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mTotal = \x1b[1;37m{result:<10}\x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     main()
 
-
 def sub():
     global num1, num2, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║        ADDITION         ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
-    num2 = int(input("     \x1b[1;32mNum2: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║       \x1b[1;33mSUBTRACTION       \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mNum2:\x1b[1;37m "))
     result = num1 - num2
     time.sleep(1)
     clear()
     sub_result()
 
-
 def sub_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║    SUBTRACTION RESULT   ║
-    ╠═════════════════════════╣
-    ║       Num1 = {num1:<10} ║
-    ║       Num2 = {num2:<10} ║
-    ║       Total = {result:<10}║
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║    \x1b[1;31mSUBTRACTION RESULT   \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║       \x1b[1;32mNum1 = \x1b[1;37m{num1:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mNum2 = \x1b[1;37m{num2:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mTotal = \x1b[1;37m{result:<10}\x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     main()
@@ -67,11 +72,11 @@ def sub_result():
 def mult():
     global num1, num2, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║      MULTIPLICATION     ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
-    num2 = int(input("     \x1b[1;32mNum2: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║      \x1b[1;33mMULTIPLICATION     \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mNum2:\x1b[1;37m "))
     result = num1 * num2
     time.sleep(1)
     clear()
@@ -79,13 +84,13 @@ def mult():
 
 def mult_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║  MULTIPLICATION RESULT  ║
-    ╠═════════════════════════╣
-    ║       Num1 = {num1:<10} ║
-    ║       Num2 = {num2:<10} ║
-    ║       Total = {result:<10}║
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║  \x1b[1;31mMULTIPLICATION RESULT  \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║       \x1b[1;32mNum1 = \x1b[1;37m{num1:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mNum2 = \x1b[1;37m{num2:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mTotal = \x1b[1;37m{result:<10}\x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     main()       
@@ -93,11 +98,11 @@ def mult_result():
 def div():
     global num1, num2, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║        DIVISION         ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
-    num2 = int(input("     \x1b[1;32mNum2: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║        \x1b[1;33mDIVISION         \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mNum2:\x1b[1;37m "))
     result = num1 / num2
     time.sleep(1)
     clear()
@@ -105,13 +110,13 @@ def div():
 
 def div_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║     DIVISION RESULT     ║
-    ╠═════════════════════════╣
-    ║       Num1 = {num1:<10} ║
-    ║       Num2 = {num2:<10} ║
-    ║       Total = {result:<10.5f}║
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║     \x1b[1;31mDIVISION RESULT     \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║       \x1b[1;32mNum1 = \x1b[1;37m{num1:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mNum2 = \x1b[1;37m{num2:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mTotal = \x1b[1;37m{result:<10.5f}\x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     main()  
@@ -119,10 +124,10 @@ def div_result():
 def qroot():
     global num1, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║       SQUARE ROOT       ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║       \x1b[1;33mSQUARE ROOT       \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
     result = math.sqrt(num1)
     time.sleep(1)
     clear()
@@ -130,12 +135,12 @@ def qroot():
 
 def qroot_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║    SQUARE ROOT RESULT   ║
-    ╠═════════════════════════╣
-    ║       Num1 = {num1:<10} ║
-    ║       Qroot = {result:<10.4f}║
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║    \x1b[1;31mSQUARE ROOT RESULT   \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║       \x1b[1;32mNum1 = \x1b[1;37m{num1:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mQroot = \x1b[1;37m{result:<10.4f}\x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     clear()
@@ -144,11 +149,11 @@ def qroot_result():
 def asmd():
     global num1, num2, result, result1, result2, result3
     print(f""" 
-    ╔═════════════════════════╗
-    ║  ALL IN ONE CALCULATOR  ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
-    num2 = int(input("     \x1b[1;32mNum2: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║  \x1b[1;33mALL IN ONE CALCULATOR  \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mNum2:\x1b[1;37m "))
     result = num1 + num2
     result1 = num1 - num2
     result2 = num1 * num2
@@ -159,17 +164,17 @@ def asmd():
 
 def asmd_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║    ALL IN ONE RESULT    ║
-    ╠═════════════════════════╣
-    ║       Num1 = {num1:<10} ║
-    ║       Num2 = {num2:<10} ║
-    ╠═════════════════════════╣
-    ║       Add = {result:<10}  ║
-    ║       Sub = {result1:<10}  ║
-    ║       Mult = {result2:<10} ║
-    ║       Div = {result3:<10}  ║
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║    \x1b[1;31mALL IN ONE RESULT    \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║       \x1b[1;32mNum1 = \x1b[1;37m{num1:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mNum2 = \x1b[1;37m{num2:<10} \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║       \x1b[1;32mAdd = \x1b[1;37m{result:<10}  \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mSub = \x1b[1;37m{result1:<10}  \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mMult = \x1b[1;37m{result2:<10} \x1b[1;36m║
+    \x1b[1;36m║       \x1b[1;32mDiv = \x1b[1;37m{result3:<10}  \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     main()
@@ -177,11 +182,11 @@ def asmd_result():
 def exponent():
     global num1, num2, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║     RAISED TO POWER     ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mBase: "))
-    num2 = int(input("     \x1b[1;32mExponent: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║     \x1b[1;33mRAISED TO POWER     \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mBase:\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mExponent:\x1b[1;37m "))
     result = math.pow(num1, num2)
     time.sleep(1)
     clear()
@@ -189,13 +194,13 @@ def exponent():
 
 def exponent_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║ {num1} RAISED TO POWER OF {num2} ║
-    ╠═════════════════════════╣
-    ║      Num1 = {num1:<10}  ║
-    ║      Num2 = {num2:<10}  ║
-    ║      Result = {result:<10.0f}║
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║ \x1b[1;32m{num1} \x1b[1;33mRAISED TO POWER OF \x1b[1;32m{num2} \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║      \x1b[1;32mNum1 = \x1b[1;37m{num1:<10}  \x1b[1;36m║
+    \x1b[1;36m║      \x1b[1;32mNum2 = \x1b[1;37m{num2:<10}  \x1b[1;36m║
+    \x1b[1;36m║      \x1b[1;32mResult = \x1b[1;37m{result:<10.0f}\x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     clear()
@@ -204,10 +209,10 @@ def exponent_result():
 def factorial():
     global num1, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║        FACTORIAL        ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║        \x1b[1;33mFACTORIAL        \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
     result = math.factorial(num1)
     time.sleep(1)
     clear()
@@ -215,12 +220,12 @@ def factorial():
 
 def factorial_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║     FACTORIAL RESULT    ║
-    ╠═════════════════════════╣
-    ║      Num1 = {num1:<10}  ║
-    ║      Result = {result:<10}║    
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║     \x1b[1;31mFACTORIAL RESULT    \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║      \x1b[1;32mNum1 = \x1b[1;37m{num1:<10}  \x1b[1;36m║
+    \x1b[1;36m║      \x1b[1;32mResult = \x1b[1;37m{result:<10}\x1b[1;36m║    
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     clear()
@@ -229,11 +234,11 @@ def factorial_result():
 def ncr():
     global num1, num2, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║        nCr METHODS      ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
-    num2 = int(input("     \x1b[1;32mNum2: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║        \x1b[1;33mnCr METHODS      \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mNum2:\x1b[1;37m "))
     result = math.comb(num1, num2)
     time.sleep(1)
     clear()
@@ -241,13 +246,13 @@ def ncr():
 
 def ncr_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║        nCr RESULT       ║
-    ╠═════════════════════════╣
-    ║      Num1 = {num1:<10}  ║
-    ║      Num2 = {num2:<10}  ║
-    ║      Result = {result:<10}║    
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║        \x1b[1;31mnCr RESULT       \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║      \x1b[1;32mNum1 = \x1b[1;37m{num1:<10}  \x1b[1;36m║
+    \x1b[1;36m║      \x1b[1;32mNum2 = \x1b[1;37m{num2:<10}  \x1b[1;36m║
+    \x1b[1;36m║      \x1b[1;32mResult = \x1b[1;37m{result:<10}\x1b[1;36m║    
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     clear()
@@ -256,11 +261,11 @@ def ncr_result():
 def modulus():
     global num1, num2, result
     print(f""" 
-    ╔═════════════════════════╗
-    ║         MODULUS         ║
-    ╚═════════════════════════╝""")
-    num1 = int(input("     \x1b[1;32mNum1: "))
-    num2 = int(input("     \x1b[1;32mNum2: "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║         \x1b[1;33mMODULUS         \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    num1 = int(input("     \x1b[1;32mNum1:\x1b[1;37m\x1b[1;37m "))
+    num2 = int(input("     \x1b[1;32mNum2:\x1b[1;37m\x1b[1;37m "))
     result = num1 % num2
     time.sleep(1)
     clear()
@@ -268,13 +273,13 @@ def modulus():
  
 def modulus_result():
     print(f""" 
-    ╔═════════════════════════╗
-    ║      MODULUS RESULT     ║
-    ╠═════════════════════════╣
-    ║      Num1 = {num1:<10}  ║
-    ║      Num2 = {num2:<10}  ║
-    ║      Result = {result:<10}║    
-    ╚═════════════════════════╝""")
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║      \x1b[1;31mMODULUS RESULT     \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║      \x1b[1;32mNum1 = \x1b[1;37m{num1:<10}  \x1b[1;36m║
+    \x1b[1;36m║      \x1b[1;32mNum2 = \x1b[1;37m{num2:<10}  \x1b[1;36m║
+    \x1b[1;36m║      \x1b[1;32mResult = \x1b[1;37m{result:<10}\x1b[1;36m║    
+    \x1b[1;36m╚═════════════════════════╝""")
     choice = input("     \x1b[1;31mPress Enter to Go Back:\x1b[1;37m ")
     time.sleep(1)
     clear()
@@ -292,7 +297,7 @@ def shift_mode():
     \x1b[1;36m║  \x1b[1;32m5.   Modulus           \x1b[1;36m║
     \x1b[1;36m║  \x1b[1;31m0.   Back              \x1b[1;36m║
     \x1b[1;36m╚═════════════════════════╝""")
-    choice = int(input("     \x1b[1;32mChoice:\x1b[1;37m	 "))
+    choice = int(input("     \x1b[1;33mChoice:\x1b[1;37m "))
     time.sleep(1)
     clear()
 
@@ -320,18 +325,18 @@ def shift_mode():
 def main():
     clear()
     print(f""" 
-    \x1b[1;36m	╔═════════════════════════╗
-    \x1b[1;36m	║  \x1b[1;33mSCIENTIFIC CALCULATOR  \x1b[1;36m║
-    \x1b[1;36m    ╠═════════════════════════╣
-    \x1b[1;36m	║  \x1b[1;32m1.   Addition          \x1b[1;36m║
-    \x1b[1;36m	║  \x1b[1;32m2.   Subtraction       \x1b[1;36m║
-    \x1b[1;36m	║  \x1b[1;32m3.   Multiplication    \x1b[1;36m║
-    \x1b[1;36m	║  \x1b[1;32m4.   Division          \x1b[1;36m║
-    \x1b[1;36m	║  \x1b[1;32m5.   All in One        \x1b[1;36m║
-    \x1b[1;36m	║  \x1b[1;32m6.   Shift-Mode        \x1b[1;36m║    
-    \x1b[1;36m	║  \x1b[1;31m0.   Exit              \x1b[1;36m║
-    \x1b[1;36m	╚═════════════════════════╝""")
-    choice = int(input("         \x1b[1;32mChoice:\x1b[1;37m "))
+    \x1b[1;36m╔═════════════════════════╗
+    \x1b[1;36m║  \x1b[1;33mSCIENTIFIC CALCULATOR  \x1b[1;36m║
+    \x1b[1;36m╠═════════════════════════╣
+    \x1b[1;36m║  \x1b[1;32m1.   Addition          \x1b[1;36m║
+    \x1b[1;36m║  \x1b[1;32m2.   Subtraction       \x1b[1;36m║
+    \x1b[1;36m║  \x1b[1;32m3.   Multiplication    \x1b[1;36m║
+    \x1b[1;36m║  \x1b[1;32m4.   Division          \x1b[1;36m║
+    \x1b[1;36m║  \x1b[1;32m5.   All in One        \x1b[1;36m║
+    \x1b[1;36m║  \x1b[1;32m6.   Shift-Mode        \x1b[1;36m║    
+    \x1b[1;36m║  \x1b[1;31m0.   Exit              \x1b[1;36m║
+    \x1b[1;36m╚═════════════════════════╝""")
+    choice = int(input("     \x1b[1;33mChoice:\x1b[1;37m "))
     time.sleep(1)
     clear()
 
